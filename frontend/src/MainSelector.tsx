de4931a70db6610c1fb81370e076c04cf92a9caf
Michael Lam <lam.michael.81201@gmail.com>
2023-11-19 13:14:52 -0800

    Added button and spacing

--- frontend/src/MainSelector.tsx
+++ frontend/src/MainSelector.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography, Select, MenuItem } from "@mui/material";
+import { Box, Button, Grid, Typography, Select, MenuItem } from "@mui/material";
+import { padding } from "@mui/system";
 import React from "react";
 
-export default function TestHeader() {
+export default function MainSelector() {
   
   return (
     <Box sx={{ width:"1000px", height:"350px", display: "flex", flexDirection:"column", borderRadius: 2, p: 2, color:"#737373", backgroundColor:"#FFFFFF"}}>
@@ -10,20 +11,42 @@ export default function TestHeader() {
       </Typography>
 
       <div>
-        <Select label="Year">
-          <MenuItem value={2022}>2022</MenuItem>
-          <MenuItem value={2023}>2023</MenuItem>
-        </Select>
+        <form>
+          <Grid container spacing={20}>
+            <Grid item>
+              <Select label="Year">
+                <MenuItem value={2022}>2022</MenuItem>
+                <MenuItem value={2023}>2023</MenuItem>
+              </Select>
+            </Grid>
 
-        <Select label="Course">
-          <MenuItem value={"CPSC210"}>CPSC210</MenuItem>
-          <MenuItem value={"CPSC213"}>CPSC213</MenuItem>
-        </Select>
+            <Grid item>
+              <Select label="Course">
+                <MenuItem value={"CPSC210"}>CPSC210</MenuItem>
+                <MenuItem value={"CPSC213"}>CPSC213</MenuItem>
+              </Select>
+            </Grid>
 
-        <Select label="Section">
-          <MenuItem value={101}>101</MenuItem>
-          <MenuItem value={102}>102</MenuItem>
-        </Select>
+            <Grid item>
+              <Select label="Section">
+                <MenuItem value={101}>101</MenuItem>
+                <MenuItem value={102}>102</MenuItem>
+              </Select>
+            </Grid>
+
+            <Grid item>
+              <Button variant="contained"
+                sx={{
+                  width: '150px', // Set the width
+                  height: '55px', // Set the height
+                  fontSize: '20px', // Set the font size
+                }}>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+          
+        </form>
 
 
       </div>
--- frontend/src/TestHeader.tsx
+++ frontend/src/TestHeader.tsx
@@ -2,7 +2,8 @@ import { Box, Typography } from "@mui/material";
 
 export function Title() {
   return (
-    <Typography variant="h1" sx = {{color:"white", textAlign:"center" , paddingBottom: "10px",
-                                      fontFamily: "arial"}}>UBCNotes</Typography>
+    <Typography variant="h1" sx = {{color:"white", textAlign:"center" , paddingBottom: "10px", fontFamily: "arial"}}>
+      UBCNotes
+    </Typography>
   );
 }
