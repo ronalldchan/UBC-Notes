e4d8d3cd7d9ccab2650289161c826b86ed26e9e5
Michael Lam <lam.michael.81201@gmail.com>
2023-11-19 11:41:11 -0800

    Added one text

--- frontend/src/TestHeader.tsx
+++ frontend/src/TestHeader.tsx
@@ -2,8 +2,10 @@ import { Box, Typography } from "@mui/material";
 
 export default function TestHeader() {
   return (
-    <Box component="div" sx={{ width:"1000px", height:"350px", display: "flex", borderRadius: 2, p: 2, color:"white", backgroundColor:"#FFFFFF"}}>
-      This is a section container
+    <Box component="div" sx={{ width:"1000px", height:"350px", display: "flex", borderRadius: 2, p: 2, backgroundColor:"#FFFFFF"}}>
+      <Typography fontSize={48} sx={({color: "#737373", paddingLeft: "10px"})}>
+         View and Submit Notes
+      </Typography>
     </Box>
   );
 }
