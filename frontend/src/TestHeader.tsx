f95bfb49bae9df953ad709a0eb24416c7ea73bc3
Michael Lam <lam.michael.81201@gmail.com>
2023-11-19 10:44:04 -0800

    Test box

--- frontend/src/TestHeader.tsx
+++ frontend/src/TestHeader.tsx
@@ -2,8 +2,8 @@ import { Box, Typography } from "@mui/material";
 
 export default function TestHeader() {
   return (
-    <Box>
-      <Typography variant="h1">test asdjfasdjf;alksdj</Typography>
+    <Box component="div" sx={{ borderRadius: 5, width: "90vw", height: 100, p: 2, color:"white", backgroundColor:"#002145"}}>
+      This is a section container
     </Box>
   );
 }
