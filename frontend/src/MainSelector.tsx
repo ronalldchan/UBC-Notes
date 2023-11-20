1b375eba0cef534e06272ff116e7ba6f7ae78a9a
Ronald Chan <ronaldchan@dhcp-128-189-214-216.ubcsecure.wireless.ubc.ca>
2023-11-19 17:22:58 -0800

    image render bottom

--- frontend/src/MainSelector.tsx
+++ frontend/src/MainSelector.tsx
@@ -1,17 +1,28 @@
-
-import { Input, FormControl, Box, Button, Grid, Typography, Select, MenuItem, InputLabel, Grow } from "@mui/material";
+import {
+  Input,
+  FormControl,
+  Box,
+  Button,
+  Grid,
+  Typography,
+  Select,
+  MenuItem,
+  InputLabel,
+  Grow,
+  Stack,
+} from "@mui/material";
 import { padding } from "@mui/system";
-import React, { ChangeEvent, useState, useEffect} from "react";
+import React, { ChangeEvent, useState, useEffect } from "react";
 import { ref, uploadBytes, getDownloadURL } from "@firebase/storage";
-import { storage, firestore,  } from "./firebase";
+import { storage, firestore } from "./firebase";
 import { addDoc, collection, getDocs, query, where } from "@firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
 import { Note } from "./Note";
-
+import { RenderNotes } from "./RenderNotes";
 
 export default function MainSelector() {
   const [img, setImg] = useState<File | null>(null);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Note[] | null>(null);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -24,27 +35,30 @@ export default function MainSelector() {
     try {
       const collectionRef = collection(firestore, "images"); // Replace 'your_collection_name' with your actual collection name
 
-      const q = query(collectionRef,
+      const q = query(
+        collectionRef,
         where("year", "==", year),
         where("course", "==", course),
-        where("section", "==", section))
+        where("section", "==", section)
+      );
 
       const querySnapshot = await getDocs(q);
       // console.log(querySnapshot);
       const doc2 = querySnapshot.docs.map((doc) => {
         let data = doc.data();
-        
+
         return new Note(data.year, data.course, data.section, data.section, data.date, data.imageUrl);
       });
+      setData(doc2);
       console.log(doc2);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const [year, setYear] = React.useState('');
-  const [course, setCourse] = React.useState('');
-  const [section, setSection] = React.useState('');
+  const [year, setYear] = React.useState("");
+  const [course, setCourse] = React.useState("");
+  const [section, setSection] = React.useState("");
 
   const dbRef = collection(firestore, "images");
 
@@ -73,48 +87,63 @@ export default function MainSelector() {
     }
   };
 
-  const handleSelectYear = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleSelectYear = (event: { target: { value: React.SetStateAction<string> } }) => {
     setYear(event.target.value);
   };
 
-  const handleSelectCourse = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleSelectCourse = (event: { target: { value: React.SetStateAction<string> } }) => {
     setCourse(event.target.value);
   };
 
-  const handleSelectSection = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleSelectSection = (event: { target: { value: React.SetStateAction<string> } }) => {
     setSection(event.target.value);
   };
-  
 
   const [isResultOpen, setIsResultOpen] = useState(false);
 
   const [buttonClickCount, setButtonClickCount] = useState(0);
 
-
   return (
     <div>
-      <Box sx={{ width:"1000px", height:"350px", display: "flex", flexDirection:"column", borderRadius: 2, p: 2, color:"#737373", backgroundColor:"#FFFFFF"}}>
-        <Typography component="div" fontSize={48} sx={({height: "150px", color: "#737373", paddingLeft: "10px"})}>
+      <Box
+        sx={{
+          width: "1000px",
+          height: "350px",
+          display: "flex",
+          flexDirection: "column",
+          borderRadius: 2,
+          p: 2,
+          color: "#737373",
+          backgroundColor: "#FFFFFF",
+        }}
+      >
+        <Typography component="div" fontSize={48} sx={{ height: "150px", color: "#737373", paddingLeft: "10px" }}>
           View and Submit Notes
         </Typography>
 
-      <div>
-        <FormControl fullWidth>
-          <Grid container spacing={5} paddingLeft={"10px"}>
-            <Grid item>
-              <FormControl>
-                <InputLabel id="yearLabel">Year</InputLabel>
-                <Select labelId="yearLabel" label="Year" sx={{ width: 150 }} value={year} onChange={handleSelectYear}>
-                  <MenuItem value={2022}>2022</MenuItem>
-                  <MenuItem value={2023}>2023</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
+        <div>
+          <FormControl fullWidth>
+            <Grid container spacing={5} paddingLeft={"10px"}>
+              <Grid item>
+                <FormControl>
+                  <InputLabel id="yearLabel">Year</InputLabel>
+                  <Select labelId="yearLabel" label="Year" sx={{ width: 150 }} value={year} onChange={handleSelectYear}>
+                    <MenuItem value={2022}>2022</MenuItem>
+                    <MenuItem value={2023}>2023</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
 
               <Grid item>
                 <FormControl>
                   <InputLabel id="courseLabel">Course</InputLabel>
-                  <Select labelId="courseLabel" label="Course" sx={{ width: 400 }} value={course} onChange={handleSelectCourse}>
+                  <Select
+                    labelId="courseLabel"
+                    label="Course"
+                    sx={{ width: 400 }}
+                    value={course}
+                    onChange={handleSelectCourse}
+                  >
                     <MenuItem value={"CPSC210"}>CPSC210</MenuItem>
                     <MenuItem value={"CPSC213"}>CPSC213</MenuItem>
                   </Select>
@@ -124,7 +153,13 @@ export default function MainSelector() {
               <Grid item>
                 <FormControl>
                   <InputLabel id="sectionLabel">Section</InputLabel>
-                  <Select labelId="sectionLabel" label="Section" sx={{ width: 150 }} value={section} onChange={handleSelectSection}>
+                  <Select
+                    labelId="sectionLabel"
+                    label="Section"
+                    sx={{ width: 150 }}
+                    value={section}
+                    onChange={handleSelectSection}
+                  >
                     <MenuItem value={101}>101</MenuItem>
                     <MenuItem value={102}>102</MenuItem>
                   </Select>
@@ -132,14 +167,15 @@ export default function MainSelector() {
               </Grid>
 
               <Grid item>
-                <Button variant="contained"
+                <Button
+                  variant="contained"
                   sx={{
-                    width: '150px', // Set the width
-                    height: '55px', // Set the height
-                    fontSize: '20px', // Set the font size
+                    width: "150px", // Set the width
+                    height: "55px", // Set the height
+                    fontSize: "20px", // Set the font size
                   }}
                   onClick={() => {
-                    handleQuery()
+                    handleQuery();
                     setIsResultOpen(true);
                     setButtonClickCount(buttonClickCount + 1);
                     /*
@@ -147,17 +183,17 @@ export default function MainSelector() {
                       alert('clicked');
                     }
                     */
-                  }}>
+                  }}
+                >
                   Submit
                 </Button>
               </Grid>
             </Grid>
-            
           </FormControl>
         </div>
       </Box>
-      <Grow in={isResultOpen}  timeout={{ enter: 500, exit: 300 }} key={buttonClickCount}>
-        
+
+      <Grow in={isResultOpen} timeout={{ enter: 500, exit: 300 }} key={buttonClickCount}>
         <Box
           sx={{
             width: "1000px",
@@ -168,12 +204,20 @@ export default function MainSelector() {
             p: 2,
             color: "#737373",
             backgroundColor: "#FFFFFF",
-            marginTop: '20px',
+            marginTop: "20px",
           }}
         >
-          <Typography>
-            Text here.
-          </Typography>
+          {data && (
+            <Stack>
+              {data.map((item) => (
+                <Box width={20}>
+                  <img src={item.imageUrl} />
+                </Box>
+              ))}
+            </Stack>
+          )}
+
+          <Typography>Text here.</Typography>
 
           <div>
             <Input type="file" onChange={handleFileChange} />
@@ -182,7 +226,7 @@ export default function MainSelector() {
             </Button>
           </div>
         </Box>
-        </Grow>
+      </Grow>
     </div>
   );
 }
